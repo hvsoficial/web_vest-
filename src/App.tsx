0f0f4579f9e5d18e2502ea281d5b91f0e310a030
hvsoficial <56373387+hvsoficial@users.noreply.github.com>
2022-06-05 12:02:25 -0300

    troca de logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import'./styles/global.css';
+import './styles/global.css';
 import './styles/pages/landing.css';
 import Routes from './routes';
 import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
-      <Routes />
+    <Routes />
   );
 }
 
